ee9e0e989115eabbb22d2443b5885cd791c011c1
kim <lsiwh37249@gmail.com>
2025-06-16 12:58:04 +0900

    conn : OlapModeling + utils.OlapModeling

---
+++ OlapModeling.py
@@ -0,0 +1,17 @@
+class OlapModeling:
+    def load(self):
+        print("Loading data...")
+
+    def dimension(self):
+        print("Building dimension tables...")
+
+    def fact(self):
+        print("Building fact tables...")
+
+    def save(self):
+        print("Saving modeled data...")
+
+    def send_slack_alert(self):
+        print("send_slack")
+
+
